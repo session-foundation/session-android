25f1187442dcee3579c51d7c7f091906958f388c
SessionHero01 <180888785+SessionHero01@users.noreply.github.com>
2025-01-20 15:25:51 +1100

    Reset last hash on promotion

--- app/src/main/java/org/thoughtcrime/securesms/groups/GroupManagerV2Impl.kt
+++ app/src/main/java/org/thoughtcrime/securesms/groups/GroupManagerV2Impl.kt
@@ -757,6 +757,10 @@ class GroupManagerV2Impl @Inject constructor(
                     configs.groupMembers.set(member)
                 }
             }
+
+            // Remove lastHash so we can receive all the messages in the past
+            lokiAPIDatabase.clearLastMessageHashes(groupId.hexString)
+            lokiAPIDatabase.clearReceivedMessageHashValues(groupId.hexString)
         }
 
         // Delete the promotion message remotely
