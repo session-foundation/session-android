12af480e13a3e6e4693649fe5b08fb05fb138810
SessionHero01 <180888785+SessionHero01@users.noreply.github.com>
2025-07-16 10:00:13 +1000

    Update JDK version to 21

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -55,8 +55,8 @@ android {
     useLibrary("org.apache.http.legacy")
 
     compileOptions {
-        sourceCompatibility = JavaVersion.VERSION_17
-        targetCompatibility = JavaVersion.VERSION_17
+        sourceCompatibility = JavaVersion.VERSION_21
+        targetCompatibility = JavaVersion.VERSION_21
     }
 
     packaging {
