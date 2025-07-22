838ff97f74a8bbe8e8c9f6013589320ee59fa03f
SessionHero01 <180888785+SessionHero01@users.noreply.github.com>
2025-07-22 17:15:16 +1000

    Comment

--- app/src/main/java/org/session/libsession/utilities/TranslatableText.kt
+++ app/src/main/java/org/session/libsession/utilities/TranslatableText.kt
@@ -4,6 +4,12 @@ import android.content.Context
 import androidx.annotation.StringRes
 import com.squareup.phrase.Phrase
 
+/**
+ * A class that represents a text with substitutions.
+ *
+ * This is mainly used for modeling the texts inside a view model so that the view model doesn't
+ * need to do any Android-specific operations.
+ */
 data class TranslatableText(
     @param:StringRes val textResId: Int,
     val substitution: Map<StringSubKey, CharSequence>? = null
