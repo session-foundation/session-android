3d85d7dcee95bb7454855d112aaf48d3fbf7f4da
SessionHero01 <180888785+SessionHero01@users.noreply.github.com>
2025-07-23 13:29:25 +1000

    Remove debug only code

--- app/src/play/kotlin/org/thoughtcrime/securesms/reviews/GooglePlayReviewsModule.kt
+++ app/src/play/kotlin/org/thoughtcrime/securesms/reviews/GooglePlayReviewsModule.kt
@@ -3,13 +3,11 @@ package org.thoughtcrime.securesms.reviews
 import android.content.Context
 import com.google.android.play.core.review.ReviewManager
 import com.google.android.play.core.review.ReviewManagerFactory
-import com.google.android.play.core.review.testing.FakeReviewManager
 import dagger.Module
 import dagger.Provides
 import dagger.hilt.InstallIn
 import dagger.hilt.android.qualifiers.ApplicationContext
 import dagger.hilt.components.SingletonComponent
-import network.loki.messenger.BuildConfig
 import javax.inject.Singleton
 
 @Module
@@ -20,10 +18,6 @@ class GooglePlayReviewsModule {
     fun reviewManager(
         @ApplicationContext context: Context,
     ): ReviewManager {
-        if (BuildConfig.DEBUG) {
-            return FakeReviewManager(context)
-        }
-
         return ReviewManagerFactory.create(context)
     }
 }
