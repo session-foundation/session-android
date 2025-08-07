1a749ec111dba15e0aa67935e58d4b9c0d358042
SessionHero01 <180888785+SessionHero01@users.noreply.github.com>
2025-08-07 11:44:22 +1000

    Imports

--- app/src/nonDebug/kotlin/org/thoughtcrime/securesms/debugmenu/DatabaseInspector.kt
+++ app/src/nonDebug/kotlin/org/thoughtcrime/securesms/debugmenu/DatabaseInspector.kt
@@ -1,20 +1,7 @@
 package org.thoughtcrime.securesms.debugmenu
 
-import dev.fanchao.sqliteviewer.model.SupportQueryable
-import dev.fanchao.sqliteviewer.startDatabaseViewerServer
-import kotlinx.coroutines.CoroutineScope
-import kotlinx.coroutines.Job
 import kotlinx.coroutines.flow.MutableStateFlow
-import kotlinx.coroutines.flow.SharingStarted
 import kotlinx.coroutines.flow.StateFlow
-import kotlinx.coroutines.flow.map
-import kotlinx.coroutines.flow.stateIn
-import kotlinx.coroutines.flow.update
-import org.thoughtcrime.securesms.database.helpers.SQLCipherOpenHelper
-import org.thoughtcrime.securesms.dependencies.ManagerScope
-import org.thoughtcrime.securesms.util.CurrentActivityObserver
-import javax.inject.Inject
-import javax.inject.Provider
 import javax.inject.Singleton
 
 @Singleton
