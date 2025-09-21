7da8b1c0c74ca5910f734a21d45fc98db85fadee
SessionHero01 <180888785+SessionHero01@users.noreply.github.com>
2025-09-22 07:22:32 +0800

    Fixed block community request not updated

--- app/src/main/java/org/session/libsession/messaging/messages/ProfileUpdateHandler.kt
+++ app/src/main/java/org/session/libsession/messaging/messages/ProfileUpdateHandler.kt
@@ -119,6 +119,9 @@ class ProfileUpdateHandler @Inject constructor(
                             profilePic = updates.pic ?: r.profilePic,
                             blocksCommunityMessagesRequests = updates.blocksCommunityMessageRequests ?: r.blocksCommunityMessagesRequests
                         )
+                    } else if (updates.blocksCommunityMessageRequests != null &&
+                            r.blocksCommunityMessagesRequests != updates.blocksCommunityMessageRequests) {
+                        r.copy(blocksCommunityMessagesRequests = updates.blocksCommunityMessageRequests)
                     } else {
                         r
                     }
