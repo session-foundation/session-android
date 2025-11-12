9789e6334e1ef6aa4327f7add03fea287305e211
ThomasSession <thomas.r@getsession.org>
2025-11-13 10:56:36 +1100

    Version number bump

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -26,8 +26,8 @@ configurations.configureEach {
     exclude(module = "commons-logging")
 }
 
-val canonicalVersionCode = 429
-val canonicalVersionName = "1.29.0"
+val canonicalVersionCode = 430
+val canonicalVersionName = "1.29.1"
 
 val postFixSize = 10
 val abiPostFix = mapOf(
