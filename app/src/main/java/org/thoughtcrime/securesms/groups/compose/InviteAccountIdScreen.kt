46dee23995348c5d301b0312d6a9c9881c490310
jbsession <jb@getsession.org>
2025-11-17 07:33:42 +0800

    Updated InviteAccountId declaration

--- app/src/main/java/org/thoughtcrime/securesms/groups/compose/InviteAccountIdScreen.kt
+++ app/src/main/java/org/thoughtcrime/securesms/groups/compose/InviteAccountIdScreen.kt
@@ -8,6 +8,7 @@ import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.Scaffold
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.collectAsState
+import androidx.compose.runtime.getValue
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
 import kotlinx.coroutines.flow.Flow
@@ -27,9 +28,11 @@ internal fun InviteAccountIdScreen(
     onHelp: () -> Unit = {},
     onSendInvite: (shareHistory: Boolean) -> Unit
 ) {
+    val uiState by viewModel.uiState.collectAsState()
+
     InviteAccountId(
         state = state,
-        inviteState = viewModel.uiState.collectAsState().value.inviteContactsDialog,
+        inviteState = uiState.inviteContactsDialog,
         qrErrors = qrErrors,
         callbacks = callbacks,
         onBack = onBack,
