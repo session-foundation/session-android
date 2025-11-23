725e66e84229c308bca600a4be6a1cb3ff1f2587
SessionHero01 <180888785+SessionHero01@users.noreply.github.com>
2025-11-24 10:16:33 +1100

    Revert unneeded changes

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -201,7 +201,7 @@ android {
             enableUnitTestCoverage = false
             signingConfig = signingConfigs.getByName("debug")
 
-//            applicationIdSuffix = ".${name}"
+            applicationIdSuffix = ".${name}"
             enablePermissiveNetworkSecurityConfig(true)
             devNetDefaultOn(false)
             setAlternativeAppName("Session Debug")
