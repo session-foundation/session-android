3e1e702b84d5c0caa839e579dcebfaa835c5c53e
SessionHero01 <180888785+SessionHero01@users.noreply.github.com>
2025-12-02 09:58:07 +1100

    Improve lifecycle handling of restoring account and a few dependency injection optimisations

--- app/src/main/java/org/thoughtcrime/securesms/onboarding/manager/CreateAccountManager.kt
+++ app/src/main/java/org/thoughtcrime/securesms/onboarding/manager/CreateAccountManager.kt
@@ -1,47 +1,44 @@
 package org.thoughtcrime.securesms.onboarding.manager
 
-import android.app.Application
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.withContext
 import network.loki.messenger.libsession_util.PRIORITY_HIDDEN
-import org.session.libsession.snode.SnodeModule
 import org.session.libsession.utilities.ConfigFactoryProtocol
-import org.session.libsession.utilities.TextSecurePreferences
 import org.session.libsignal.database.LokiAPIDatabaseProtocol
 import org.thoughtcrime.securesms.auth.LoggedInState
 import org.thoughtcrime.securesms.auth.LoginStateRepository
 import org.thoughtcrime.securesms.database.ReceivedMessageHashDatabase
 import org.thoughtcrime.securesms.util.VersionDataFetcher
 import javax.inject.Inject
-import javax.inject.Singleton
 
-@Singleton
 class CreateAccountManager @Inject constructor(
     private val versionDataFetcher: VersionDataFetcher,
     private val configFactory: ConfigFactoryProtocol,
     private val receivedMessageHashDatabase: ReceivedMessageHashDatabase,
     private val loginStateRepository: LoginStateRepository,
+    private val database: LokiAPIDatabaseProtocol,
 ) {
-    private val database: LokiAPIDatabaseProtocol
-        get() = SnodeModule.shared.storage
+    suspend fun createAccount(displayName: String) {
+        withContext(Dispatchers.Default) {
+            // This is here to resolve a case where the app restarts before a user completes onboarding
+            // which can result in an invalid database state
+            database.clearAllLastMessageHashes()
+            receivedMessageHashDatabase.removeAll()
 
-    fun createAccount(displayName: String) {
-        // This is here to resolve a case where the app restarts before a user completes onboarding
-        // which can result in an invalid database state
-        database.clearAllLastMessageHashes()
-        receivedMessageHashDatabase.removeAll()
+            loginStateRepository.update { oldState ->
+                require(oldState == null) {
+                    "Attempting to create a new account when one already exists!"
+                }
 
-        loginStateRepository.update { oldState ->
-            require(oldState == null) {
-                "Attempting to create a new account when one already exists!"
+                LoggedInState.generate(seed = null)
             }
 
-            LoggedInState.generate(seed = null)
-        }
+            configFactory.withMutableUserConfigs {
+                it.userProfile.setName(displayName)
+                it.userProfile.setNtsPriority(PRIORITY_HIDDEN)
+            }
 
-        configFactory.withMutableUserConfigs {
-            it.userProfile.setName(displayName)
-            it.userProfile.setNtsPriority(PRIORITY_HIDDEN)
+            versionDataFetcher.startTimedVersionCheck()
         }
-
-        versionDataFetcher.startTimedVersionCheck()
     }
 }
--- app/src/main/java/org/thoughtcrime/securesms/onboarding/manager/LoadAccountManager.kt
+++ app/src/main/java/org/thoughtcrime/securesms/onboarding/manager/LoadAccountManager.kt
@@ -1,11 +1,7 @@
 package org.thoughtcrime.securesms.onboarding.manager
 
-import android.content.Context
-import kotlinx.coroutines.CoroutineScope
 import kotlinx.coroutines.Dispatchers
-import kotlinx.coroutines.Job
-import kotlinx.coroutines.launch
-import org.session.libsession.snode.SnodeModule
+import kotlinx.coroutines.withContext
 import org.session.libsession.utilities.TextSecurePreferences
 import org.session.libsignal.database.LokiAPIDatabaseProtocol
 import org.thoughtcrime.securesms.auth.LoggedInState
@@ -13,28 +9,17 @@ import org.thoughtcrime.securesms.auth.LoginStateRepository
 import org.thoughtcrime.securesms.database.ReceivedMessageHashDatabase
 import org.thoughtcrime.securesms.util.VersionDataFetcher
 import javax.inject.Inject
-import javax.inject.Singleton
 
-@Singleton
 class LoadAccountManager @Inject constructor(
-    @param:dagger.hilt.android.qualifiers.ApplicationContext private val context: Context,
     private val prefs: TextSecurePreferences,
     private val versionDataFetcher: VersionDataFetcher,
     private val receivedMessageHashDatabase: ReceivedMessageHashDatabase,
     private val loginStateRepository: LoginStateRepository,
-) {
     private val database: LokiAPIDatabaseProtocol
-        get() = SnodeModule.shared.storage
+) {
 
-    private var restoreJob: Job? = null
-
-    private val scope = CoroutineScope(Dispatchers.IO)
-
-    fun load(seed: ByteArray) {
-        // only have one sync job running at a time (prevent QR from trying to spawn a new job)
-        if (restoreJob?.isActive == true) return
-
-        restoreJob = scope.launch {
+    suspend fun load(seed: ByteArray) {
+        withContext(Dispatchers.Default) {
             // This is here to resolve a case where the app restarts before a user completes onboarding
             // which can result in an invalid database state
             database.clearAllLastMessageHashes()
