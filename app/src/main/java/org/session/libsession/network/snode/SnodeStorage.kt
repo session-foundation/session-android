02b7159bbf8ec14d950151478217667e9fd97699
ThomasSession <thomas.r@getsession.org>
2025-12-17 16:45:37 +1100

    interface implementations

--- app/src/main/java/org/session/libsession/network/snode/SnodeStorage.kt
+++ app/src/main/java/org/session/libsession/network/snode/SnodeStorage.kt
@@ -3,6 +3,9 @@ package org.session.libsession.network.snode
 
 import org.session.libsession.network.model.Path
 import org.session.libsignal.utilities.Snode
+import org.thoughtcrime.securesms.database.LokiAPIDatabase
+
+//todo ONION  need a hilt module to inject all of these
 
 interface SnodePathStorage {
     fun getOnionRequestPaths(): List<Path>
@@ -18,4 +21,39 @@ interface SwarmStorage {
 interface SnodePoolStorage {
     fun getSnodePool(): Set<Snode>
     fun setSnodePool(newValue: Set<Snode>)
+}
+
+
+class DbSnodePathStorage(private val db: LokiAPIDatabase) : SnodePathStorage {
+    override fun getOnionRequestPaths(): List<Path> {
+        return db.getOnionRequestPaths()
+    }
+
+    override fun setOnionRequestPaths(paths: List<Path>) {
+        db.setOnionRequestPaths(paths)
+    }
+
+    override fun clearOnionRequestPaths() {
+        db.clearOnionRequestPaths()
+    }
+}
+
+class DbSwarmStorage(private val db: LokiAPIDatabase) : SwarmStorage {
+    override fun getSwarm(publicKey: String): Set<Snode> {
+        return db.getSwarm(publicKey) ?: emptySet() // Handle potential null return
+    }
+
+    override fun setSwarm(publicKey: String, swarm: Set<Snode>) {
+        db.setSwarm(publicKey, swarm)
+    }
+}
+
+class DbSnodePoolStorage(private val db: LokiAPIDatabase) : SnodePoolStorage {
+    override fun getSnodePool(): Set<Snode> {
+        return db.getSnodePool()
+    }
+
+    override fun setSnodePool(newValue: Set<Snode>) {
+        db.setSnodePool(newValue)
+    }
 }
