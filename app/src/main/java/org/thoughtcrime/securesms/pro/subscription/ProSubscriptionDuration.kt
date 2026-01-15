1edee925761a53ddc76a544c52ee2f9de9c3f2a2
ThomasSession <thomas.r@getsession.org>
2026-01-15 12:31:17 +1100

    SES-5072, SES-5077, SES-5075, SES-5078,  SES-5118 - Date formatting changes

--- app/src/main/java/org/thoughtcrime/securesms/pro/subscription/ProSubscriptionDuration.kt
+++ app/src/main/java/org/thoughtcrime/securesms/pro/subscription/ProSubscriptionDuration.kt
@@ -13,19 +13,3 @@ enum class ProSubscriptionDuration(val duration: Period, val id: String) {
 
 fun ProSubscriptionDuration.getById(id: String): ProSubscriptionDuration? =
     ProSubscriptionDuration.entries.find { it.id == id }
-
-private const val proSettingsDateFormat = "MMMM d, yyyy"
-
-fun ProSubscriptionDuration.expiryFromNow(now: Instant = Instant.now()): String {
-    // It's important to convert the Instant to a ZonedDateTime as adding
-    // a Period like Month depends on the timezone (daylight savings etc).
-    val newSubscriptionExpiryDate = now
-        .atZone(ZoneId.systemDefault())
-        .plus(duration)
-        .toInstant()
-        .toEpochMilli()
-
-    return DateUtils.getLocaleFormattedDate(
-        newSubscriptionExpiryDate, proSettingsDateFormat
-    )
-}
