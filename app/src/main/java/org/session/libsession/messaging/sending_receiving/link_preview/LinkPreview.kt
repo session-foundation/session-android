ad26aba356a380276d1488dc473e5b73deae10c3
SessionHero01 <180888785+SessionHero01@users.noreply.github.com>
2026-02-13 10:54:55 +1100

    Remove jackson related for linkpreview

--- app/src/main/java/org/session/libsession/messaging/sending_receiving/link_preview/LinkPreview.kt
+++ app/src/main/java/org/session/libsession/messaging/sending_receiving/link_preview/LinkPreview.kt
@@ -1,37 +1,19 @@
 package org.session.libsession.messaging.sending_receiving.link_preview
 
-import com.fasterxml.jackson.annotation.JsonIgnore
-import com.fasterxml.jackson.annotation.JsonProperty
+import kotlinx.serialization.Serializable
+import kotlinx.serialization.Transient
 import org.session.libsession.messaging.sending_receiving.attachments.Attachment
 import org.session.libsession.messaging.sending_receiving.attachments.AttachmentId
 import org.session.libsession.messaging.sending_receiving.attachments.DatabaseAttachment
-import org.session.libsignal.utilities.JsonUtil
-import java.io.IOException
-import java.util.Objects
 
-class LinkPreview (
-
-    @param:JsonProperty("url")
-    @field:JsonProperty("url")
+@Serializable
+data class LinkPreview (
     val url: String,
-
-    @param:JsonProperty("title")
-    @field:JsonProperty("title")
     val title: String,
-
-    @param:JsonProperty("attachmentId")
-    @field:JsonProperty("attachmentId")
-    val attachmentId: AttachmentId? = null
+    val attachmentId: AttachmentId? = null,
+    @Transient
+    val thumbnail: Attachment? = null,
 ) {
-
-    /**
-     * Not serialized.
-     * Only used in-memory when we already have the thumbnail.
-     */
-    @JsonIgnore
-    var thumbnail: Attachment? = null
-        private set
-
     /**
      * Constructor when we already have a DatabaseAttachment thumbnail.
      */
@@ -42,10 +24,9 @@ class LinkPreview (
     ) : this(
         url = url,
         title = title,
-        attachmentId = thumbnail.attachmentId
-    ) {
-        this.thumbnail = thumbnail
-    }
+        attachmentId = thumbnail.attachmentId,
+        thumbnail = thumbnail,
+    )
 
     /**
      * Constructor when we already have an Attachment (nullable).
@@ -57,31 +38,8 @@ class LinkPreview (
     ) : this(
         url = url,
         title = title,
-        attachmentId = null
-    ) {
-        this.thumbnail = thumbnail
-    }
+        attachmentId = null,
+        thumbnail = thumbnail,
+    )
 
-    @Throws(IOException::class)
-    fun serialize(): String {
-        return JsonUtil.toJsonThrows(this)
-    }
-
-    companion object {
-        @Throws(IOException::class)
-        fun deserialize(serialized: String): LinkPreview {
-            return JsonUtil.fromJson(serialized, LinkPreview::class.java)
-        }
-    }
-
-    override fun equals(other: Any?): Boolean {
-        if (this === other) return true
-        if (other == null || javaClass != other.javaClass) return false
-        val that = other as LinkPreview
-        return url == that.url && title == that.title && attachmentId == that.attachmentId && thumbnail == that.thumbnail
-    }
-
-    override fun hashCode(): Int {
-        return Objects.hash(url, title, attachmentId, thumbnail)
-    }
 }
