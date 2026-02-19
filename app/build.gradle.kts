dac869305a04da22723fc9b16378fd5592bd089e
SessionHero01 <180888785+SessionHero01@users.noreply.github.com>
2026-02-20 09:19:24 +1100

    Bump version to 1.31.3

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -26,8 +26,8 @@ configurations.configureEach {
     exclude(module = "commons-logging")
 }
 
-val canonicalVersionCode = 439
-val canonicalVersionName = "1.31.2"
+val canonicalVersionCode = 440
+val canonicalVersionName = "1.31.3"
 
 val postFixSize = 10
 val abiPostFix = mapOf(
