15e0814f47f735c517730fa1754eceaca5728d39
ThomasSession <thomas.r@getsession.org>
2026-03-23 14:15:37 +1100

    Better divided amongst flavours

--- app/src/play/kotlin/org/thoughtcrime/securesms/onboarding/messagenotifications/FastModeAvailability.kt
+++
@@ -1,9 +0,0 @@
-package org.thoughtcrime.securesms.onboarding.messagenotifications
-
-import android.app.Application
-import com.google.android.gms.common.ConnectionResult
-import com.google.android.gms.common.GoogleApiAvailability
-
-internal fun Application.isFastModeAvailable(): Boolean {
-    return GoogleApiAvailability.getInstance().isGooglePlayServicesAvailable(this) == ConnectionResult.SUCCESS
-}
