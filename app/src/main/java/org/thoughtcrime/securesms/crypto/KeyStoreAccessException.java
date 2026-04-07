a7385bc0097148c7e64e05f9d9c470201f556f99
ThomasSession <thomas.r@getsession.org>
2026-04-07 10:21:48 +1000

    Reverting DB migration error plug

--- app/src/main/java/org/thoughtcrime/securesms/crypto/KeyStoreAccessException.java
+++
@@ -1,8 +0,0 @@
-package org.thoughtcrime.securesms.crypto;
-
-public final class KeyStoreAccessException extends RuntimeException {
-
-  public KeyStoreAccessException(String message, Throwable cause) {
-    super(message, cause);
-  }
-}
