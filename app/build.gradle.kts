f6282d61d1de29883d9cffbe0f4fceba26740b9f
Morgan Pretty <morgan.t.pretty@gmail.com>
2026-09-02 09:08:21 +1000

    Bump version to 1.34.0 (452)

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -26,8 +26,8 @@ configurations.configureEach {
     exclude(module = "commons-logging")
 }
 
-val canonicalVersionCode = 451
-val canonicalVersionName = "1.33.4"
+val canonicalVersionCode = 452
+val canonicalVersionName = "1.34.0"
 
 val postFixSize = 10
 val abiPostFix = mapOf(
